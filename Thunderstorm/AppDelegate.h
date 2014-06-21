9768a981d94c6d93597478fc25dce6cd1327c871
Darshan Shankar <d.n.shankar@gmail.com>
2014-06-21 05:03:49 -0700

    Initial Commit

---
+++ Thunderstorm/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Thunderstorm
+//
+//  Created by Darshan Shankar on 6/21/14.
+//  Copyright (c) 2014 Darshan Shankar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Thunderstorm/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Thunderstorm
+//
+//  Created by Darshan Shankar on 6/21/14.
+//  Copyright (c) 2014 Darshan Shankar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
